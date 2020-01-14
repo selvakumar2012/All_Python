ce61c9483acf6dc6e6c9c76e9e19030671447e21
Subbaiah <Selvakumar.Subbaiah@emc.com>
2020-01-14 11:57:21 +0530

    Message: Test

---
+++ abc.py
@@ -0,0 +1,12 @@
+ Volume in drive C is OS
+ Volume Serial Number is 22C5-5B42
+
+ Directory of C:\Selvakumar\Git\All_Python
+
+01/14/2020  11:57 AM    <DIR>          .
+01/14/2020  11:57 AM    <DIR>          ..
+01/14/2020  11:57 AM                 0 abc.py
+01/14/2020  11:55 AM                12 README.md
+01/14/2020  11:55 AM                23 test1.txt
+               3 File(s)             35 bytes
+               2 Dir(s)  292,401,811,456 bytes free
